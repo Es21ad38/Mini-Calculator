c75e6c822fa6539abf0ba6baa0a4e8afed76db72
Es21ad38 <gokulnath7879@gmail.com>
2025-05-29 22:15:53 +0530

    Add files via upload

--- Mini Calculator.py
+++ Mini Calculator.py
@@ -1,67 +1,88 @@
 while True:
     print("~~Welcome To Online Calculator~~")
+    print()
     a = int(input("How Many Values You Want to Perform: "))
+    print()
     b = int(input("Click the Valid Number to Do a Operations 1 - Addition, 2 - Subtracton, 3 - Multiplication, 4 - Division, 5 - Modulus: "))
+    print()
     lst = []
-    match b:
-        case 1:
-            for i in range(a):
-                num = int(input(f"Enter a {i}th Number: "))
-                lst.append(num)
+    if (a>=2):
+        match b:
+                case 1:
+                    for i in range(a):
+                        num = int(input(f"Enter a {i}th Number: "))
+                        lst.append(num)
 
-            sum1 = 0
-            for j in range(a):
-                sum1 += lst[j]
-            print("Addition of Numbers is: ",sum1)
-            
-        case 2:
-            for i in range(a):
-                num = int(input(f"Enter a {i}th Number: "))
-                lst.append(num)
-                
+                    temp1 = 0
+                    for j in range(a):
+                        temp1 += lst[j]
+                        print()
+                    print("Addition of Numbers is: ",temp1)
+                    print()
+                    
+                case 2:
+                    for i in range(a):
+                        num = int(input(f"Enter a {i}th Number: "))
+                        lst.append(num)
+                        
 
-            sum1 = 0
-            sum2 = lst[0]
-            for j in range(1,a):
-                sum1 = lst[0]
-                sum2 -= lst[j]
-            print("Subtraction of Numbers is: ",sum2)
+                    temp1 = 0
+                    temp2 = lst[0]
+                    for j in range(1,a):
+                        temp1 = lst[0]
+                        temp2 -= lst[j]
+                        print()
+                    print("Subtraction of Numbers is: ",temp2)
 
-        case 3:
-            for i in range(a):
-                num = int(input(f"Enter a {i}th Number: "))
-                lst.append(num)
-                
+                case 3:
+                    for i in range(a):
+                        num = int(input(f"Enter a {i}th Number: "))
+                        lst.append(num)
+                        
 
-            sum1 = 1
-            for j in range(a):
-                sum1 *= lst[j]
-            print("Multiplication of Numbers is: ",sum1)
+                    temp1 = 1
+                    for j in range(a):
+                        temp1 *= lst[j]
+                        print()
+                    print("Multiplication of Numbers is: ",temp1)
+                    print()
 
-        case 4:
-            for i in range(a):
-                num = int(input(f"Enter a {i}th Number: "))
-                lst.append(num)
-                
-            sum1 = 0
-            sum2 = lst[0]
-            for j in range(1,a):
-                sum1 = lst[0]
-                sum2 /= lst[j]
-            print("Division of Numbers is: ",sum2)
+                case 4:
+                    for i in range(a):
+                        num = int(input(f"Enter a {i}th Number: "))
+                        lst.append(num)
+                        
+                    temp1 = 0
+                    temp2 = lst[0]
+                    for j in range(1,a):
+                        temp1 = lst[0]
+                        temp2 /= lst[j]
+                        print()
+                    print("Division of Numbers is: ",temp2)
+                    print()
 
-        case 5:
-            for i in range(a):
-                num = int(input(f"Enter a {i}th Number: "))
-                lst.append(num)
-                
-            sum1 = 0
-            sum2 = lst[0]
-            for j in range(1,a):
-                sum1 = lst[0]
-                sum2 %= lst[j]
-            print("Modulus of Numbers is: ",sum2)
-            
+                case 5:
+                    for i in range(a):
+                        num = int(input(f"Enter a {i}th Number: "))
+                        lst.append(num)
+                        
+                    temp1 = 0
+                    temp2 = lst[0]
+                    for j in range(1,a):
+                        temp1 = lst[0]
+                        temp2 %= lst[j]
+                        print()
+                    print("Modulus of Numbers is: ",temp2)
+                    print()
+                    
+                case _:
+                    print()
+                    print("Enter a Valid Input to Perform a Operation")
+                    print()
+    else:
+        print()
+        print("Enter a Minimum of 2 Values to Perform the Operations! ")
+        print()
 
         
 
